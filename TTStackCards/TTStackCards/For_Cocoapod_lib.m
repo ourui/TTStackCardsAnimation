896ae1ab80ed3bf1a5a2853085b4d3abc88772d3
wangrui <wangrui@wangruideMac-mini.local>
2015-02-03 15:03:15 +0800

    add a cocoa pod class file

---
+++ TTStackCards/TTStackCards/For_Cocoapod_lib.m
@@ -0,0 +1,13 @@
+//
+//  For_Cocoapod_lib.m
+//  TTStackCards
+//
+//  Created by wangrui on 15-2-3.
+//  Copyright (c) 2015年 wangrui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@implementation For_Cocoapod_lib : NSObject 
+
+@end
